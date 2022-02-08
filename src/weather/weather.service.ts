5995abd7721c9f7cbfdf98c70e2f7a8094733a1a
Нуртилек Кошкар <nurtylek98@gmail.com>
2022-02-08 22:10:50 +0600

    add types =)

--- src/weather/weather.service.ts
+++ src/weather/weather.service.ts
@@ -3,13 +3,12 @@ import {
   InternalServerErrorException,
   Logger,
 } from '@nestjs/common';
-import {
-  WeatherFilterDto,
-} from './dto/weather-filter.dto';
+import { WeatherFilterDto } from './dto/weather-filter.dto';
 import { HttpService } from '@nestjs/axios';
 import { ConfigService } from '@nestjs/config';
 import { catchError, map, Observable } from 'rxjs';
 import { Units } from './models/units.enum';
+import { WeatherResponseModel, Wind } from './models/weather.model';
 
 @Injectable()
 export class WeatherService {
@@ -23,14 +22,16 @@ export class WeatherService {
     private readonly configService: ConfigService,
   ) {}
 
-  getCityWeatherByCityName(filterQuery: WeatherFilterDto): Observable<any> {
+  getCityWeatherByCityName(
+    filterQuery: WeatherFilterDto,
+  ): Observable<WeatherResponseModel> {
     const url = encodeURI(
       `${this.#apiUrl}?appid=${this.#apiAppId}&q=${filterQuery.name}&units=${
         filterQuery.units ?? Units.METRIC
       }`,
     );
 
-    return this.http.get(url).pipe(
+    return this.http.get<WeatherResponseModel>(url).pipe(
       map((res) => res.data),
       catchError((err) => {
         this.logger.error('Error while getting weather: ', err);
@@ -41,13 +42,14 @@ export class WeatherService {
     );
   }
 
-  getCityWindSpeedByCityName(filterQuery: WeatherFilterDto) {
+  getCityWindSpeedByCityName(filterQuery: WeatherFilterDto): Observable<Wind> {
     const url = encodeURI(
       `${this.#apiUrl}?appid=${this.#apiAppId}&q=${filterQuery.name}&units=${
         filterQuery.units ?? Units.METRIC
       }`,
     );
-    return this.http.get(url).pipe(
+
+    return this.http.get<WeatherResponseModel>(url).pipe(
       map((res) => res.data),
       map((weather) => weather.wind),
       catchError((err) => {
